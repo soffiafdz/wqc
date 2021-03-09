8368b054a6a42c81fc24193c0ef7fda729390349
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2021-03-09 12:12:59 -0500

    Changed path to work under different location

--- test_wqc.sh
+++ test_wqc.sh
@@ -2,6 +2,6 @@
 
 source /ipl/quarantine/conda/etc/profile.d/conda.sh
 conda activate wqc
-~preventAD/wqc/wqc.py \
+~/preventAD/wqc/wqc.py \
 	--csv ~/preventAD/qc.csv \
 	~/preventAD/proc_20210212/*/qc/*.jpg
